6d64d5e724fcdb06fbfabb38b0637b887d12c979
Philip Tran <tnvphuccntt@gmail.com>
2026-07-11 10:12:42 +0700

    reconfig vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,39 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ],
+//   server: {
+//     host: true, 
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://user-service:8081',
+//         changeOrigin: true,
+//       },
+//       '/class-api': {
+//         target: 'http://class-service:8082',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/class-api/, '/api'),
+//       },
+//     },
+//   },
+// })
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: true,
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://user-service:8081',
+        target: 'http://localhost:8081',
         changeOrigin: true,
       },
       '/class-api': {
-        target: 'http://class-service:8082',
+        target: 'http://localhost:8082',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/class-api/, '/api'),
       },
